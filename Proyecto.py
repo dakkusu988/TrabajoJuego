4f8e548c456912a307e2d2c67739351d1ab5e0f6
David <93660749+dakkusu988@users.noreply.github.com>
2023-10-19 11:26:53 +0200

    Add files via upload

--- Proyecto.py
+++ Proyecto.py
@@ -7,10 +7,22 @@ datos = file.json()
 # Obtener la lista de logros
 logros = datos["logros"]
 
+# Función para mostrar un logro con todos sus detalles
+def mostrar_logro(logro, mostrar_fecha=True):
+    print()
+    print(f"Logro: {logro['nombre']}")
+    print(f"Descripción: {logro['descripción']}")
+    print(f"Progreso: {logro['progreso']}/{logro['meta']}")  # Mostrar progreso y meta
+    print(f"Desbloqueado: {logro['desbloqueado']}")
+    
+    if mostrar_fecha and logro['desbloqueado'] and logro['fechaDesbloqueo']:
+        print(f"Fecha de desbloqueo: {logro['fechaDesbloqueo']}")
+
 # Función para buscar logros por nombre
 def buscar_por_nombre():
     busqueda = input("Ingrese el nombre o parte del nombre del logro: ")
     print(f"Logros que contienen '{busqueda}':")
+    
     for logro in logros:
         if busqueda.lower() in logro['nombre'].lower():
             mostrar_logro(logro)
@@ -19,14 +31,18 @@ def buscar_por_nombre():
 def mostrar_todos():
     print("Todos los logros:")
     print()
+
     print("LOGROS DESBLOQUEADOS:")
     print("----------------------")
+    
     for logro in logros:
         if logro['desbloqueado']:
             mostrar_logro(logro)
+
     print()
     print("LOGROS BLOQUEADOS:")
     print("----------------------")
+    
     for logro in logros:
         if not logro['desbloqueado']:
             mostrar_logro(logro, mostrar_fecha=False)
@@ -36,24 +52,15 @@ def filtrar_por_progreso():
     print("Logros no desbloqueados con progreso de mayor a menor:")
     logros_no_desbloqueados = [logro for logro in logros if not logro['desbloqueado']]
     logros_no_desbloqueados.sort(key=lambda x: x['progreso'], reverse=True)
+    
     for logro in logros_no_desbloqueados:
         mostrar_logro(logro)
-
-# Función para mostrar un logro con todos sus detalles (incluyendo progreso y meta)
-def mostrar_logro(logro, mostrar_fecha=True):
-    print()
-    print(f"Logro: {logro['nombre']}")
-    print(f"Descripción: {logro['descripción']}")
-    print(f"Progreso: {logro['progreso']}/{logro['meta']}")  # Mostrar progreso y meta
-    print(f"Desbloqueado: {logro['desbloqueado']}")
-    if mostrar_fecha and logro['desbloqueado'] and logro['fechaDesbloqueo']:
-        print(f"Fecha de desbloqueo: {logro['fechaDesbloqueo']}")
 
 while True:
     # Agrega una línea de separación
     print("=========================================")
 
-    # Solicitar información al usuario
+    # Solicita información al usuario
     print("Elija una opción:")
     print("1. Filtrar logros por nombre")
     print("2. Mostrar todos los logros")
@@ -72,10 +79,10 @@ while True:
         "4": lambda: exit(),
     }
 
-    # Obtener la función correspondiente a la opción ingresada
+    # Obtiene la función correspondiente a la opción ingresada
     funcion = opciones.get(opcion, lambda: print("Opción no válida. Por favor, ingrese un número válido de 1 a 4."))
     print()  # Agrega un espacio en blanco
 
-    # Ejecutar la función
+    # Ejecuta la función
     funcion()
     print()  # Agrega un espacio en blanco
